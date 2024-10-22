b2a7365b8073c498a6f2e1dc841e1fe12ed30a68
sulochan barakoti <sulochanbarakoti@gmail.com>
2024-10-22 21:27:51 +0300

    "Rename 'routes' to 'pages' and add errorElement to root route"

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./routes/root";
+import Root from "./pages/root";
 import Navbar from "./components/nav";
-import Destination from "./routes/destination";
-import Activities from "./routes/activities";
-import PostDetails from "./routes/postDetails";
+import Destination from "./pages/destination";
+import Activities from "./pages/activities";
+import PostDetails from "./pages/postDetails";
 import Footer from "./components/footer";
+import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Root /> },
+  { path: "/", element: <Root />, errorElement: <ErrorPage /> },
   { path: "/destination", element: <Destination /> },
   { path: "/activities", element: <Activities /> },
   { path: "/post/:id", element: <PostDetails /> },
