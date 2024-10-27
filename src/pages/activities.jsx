1443c278a8e24e260de1f8ae576c32190206e090
sulochan barakoti <sulochanbarakoti@gmail.com>
2024-10-27 12:51:51 +0200

    "Uncommented two lines of code in Activities component"

--- src/pages/activities.jsx
+++ src/pages/activities.jsx
@@ -3,8 +3,8 @@ import images from "../assets/images";
 const Activities = () => {
   return (
     <div className="h-[100vh] w-full flex flex-col justify-center items-center space-y-5">
-      {/* <div className="text-2xl font-bold ">This page is in progress.....</div>
-      <img src={images.working} alt="" /> */}
+      <div className="text-2xl font-bold ">This page is in progress.....</div>
+      <img src={images.working} alt="" />
     </div>
   );
 };
